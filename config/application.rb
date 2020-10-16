4becb65336de5a4d2895b0bb4bb1e56142e7de2b
Bryant Richards <dragonseyeimages@gmail.com>
2020-10-16 13:37:54 -0500

    ENV config

--- config/application.rb
+++ config/application.rb
@@ -1,7 +1,9 @@
+# frozen_string_literal: true
+
 require_relative 'boot'
 
 require 'rails/all'
-#require 'binding.pry'
+# require 'binding.pry'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -10,6 +12,14 @@ Bundler.require(*Rails.groups)
 module InASnap
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
+    config.before_configuration do
+      env_file = File.join(Rails.root, 'config', 'local_env.yml')
+      if File.exist?(env_file)
+        YAML.safe_load(File.open(env_file)).each do |key, value|
+          ENV[key.to_s] = value
+        end
+      end
+    end
     config.load_defaults 6.0
 
     # Settings in config/environments/* take precedence over those specified here.
