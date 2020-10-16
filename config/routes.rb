12cb01cbf96a280e3799560d53dd3fce8b9299c7
Bryant Richards <dragonseyeimages@gmail.com>
2020-10-16 13:30:46 -0500

    installed omniauth

--- config/routes.rb
+++ config/routes.rb
@@ -5,26 +5,26 @@ Rails.application.routes.draw do
   resources :users
   resources :photographers do
     resources :reviews, :only => [:create, :new, :index, :update]
-  end 
-  resources :reviews, :only => [:show, :index, :update, :edit, :destroy] 
+  end
+  resources :reviews, :only => [:show, :index, :update, :edit, :destroy]
   resources :photographers, :except => [:create, :edit, :delete]
- 
+
   #devise_for :users, controllers: {omniauth_callbacks: 'user/omniauth_callbacks'}
   get 'welcome', to: 'static#welcome'
-  
+
   get '/signup', to: 'users#new'
   post '/signup', to: 'users#create'
-  
+
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create', as: 'login_user'
 
   get '/auth/:provider/callback', to: 'sessions#create', via: [:get, :post]
-  #get 'auth/facebook', as: 'facebook_login'
+  get 'auth/facebook', as: 'facebook_login'
   #get '/auth/:provider/callback', to: 'sessions#create', via: [:get, :post]
-  
+
   get '/users', to: 'photographers#index'
   post '/users/:id', to: 'photographers#index'
-  
+
   delete '/logout', to: 'sessions#destroy'
 =begin
 
@@ -33,7 +33,7 @@ Rails.application.routes.draw do
 
     get '/photographers/:photographer_id/reviews', to: 'photographers#show'
     post '/photographers/:photographer_id/reviews' => CREATE
-    
+
 
     get '/reviews', to: 'reviews#index'
     get '/reviews/new', to: 'reviews#new'
@@ -43,12 +43,12 @@ Rails.application.routes.draw do
 
     get '/reviews/:id/edit', to: 'reviews#edit'
     patch 'reviews/:id', to: 'reviews#update'
-=end 
+=end
 
    # resources :users, only: [:index]
-    #resources :photographers do 
+    #resources :photographers do
      # resources :reviews, only: [:new, :create, :index]
-    #end 
+    #end
     #resources :reviews, only: [:index, :show, :new, :create, :edit, :update]
 
 
